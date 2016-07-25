28dc27f1e29cff8ffd31883bc8d4c86cf19d908d
haorenyk <haorenyk@126.com>
2016-07-25 16:20:47 +0800

    第二次提交项目

---
+++ maven_baby/src/main/java/cn/hp/add/Demo01.java
@@ -0,0 +1,12 @@
+package cn.hp.add;
+
+public class Demo01 {
+
+public static void main(String[] args) {
+	System.out.println("呵呵呵呵呵呵");
+	System.out.println("嘿嘿嘿额黑");
+	System.out.println("ooo o oo o");
+	System.out.println("ppppppppppppp");
+	System.out.println("hahahahahaah s");
+}
+}
